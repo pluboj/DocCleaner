5ad5c700edb967ad6014577e3b4dfced8092f2a7
pluboj <pluboj@yahoo.com>
2017-11-17 16:55:23 -0800

    Update App

--- Main.py
+++ Main.py
@@ -6,13 +6,13 @@ class App:
     def __init__(self, master):
         master.minsize(width=520, height=100)
         master.maxsize(width=520, height=100)
-        parent.title('DocCleaner v1.0')
+        master.title('DocCleaner v1.0')
 
-        Label(parent, text="Directory:").grid(row=0, column=0, sticky='e')
-        Entry(parent, width=60).grid(row=0, column=1,
+        Label(master, text="Directory:").grid(row=0, column=0, sticky='e')
+        Entry(master, width=60).grid(row=0, column=1,
                                      padx=2, pady=2, sticky='we', columnspan=9)
-        Button(parent, text="Select File", command=self.open_file).grid(row=0, column=10,
-                                                                   sticky='e' + 'w', padx=10, pady=2)
+        Button(master, text="Select File",
+               command=self.open_file).grid(row=0, column=10, sticky='e' + 'w', padx=10, pady=2)
 
     @staticmethod
     def open_file():
