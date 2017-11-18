6df24d80a0e40d0a0841cd3e99927c60aeb45b33
pluboj <pluboj@yahoo.com>
2017-11-17 22:52:49 -0800

    Add filechooser

--- Main.py
+++ Main.py
@@ -3,22 +3,30 @@ import tkinter.filedialog
 
 
 class App:
+    file_name = None
+    input_dir = None
+
     def __init__(self, master):
         master.minsize(width=520, height=100)
         master.maxsize(width=520, height=100)
         master.title('DocCleaner v1.0')
 
         Label(master, text="Directory:").grid(row=0, column=0, sticky='e')
-        Entry(master, width=60).grid(row=0, column=1,
-                                     padx=2, pady=2, sticky='we', columnspan=9)
+        global input_dir
+        input_dir = Entry(master, width=60)
+        input_dir.grid(row=0, column=1, padx=2, pady=2, sticky='we', columnspan=9)
         Button(master, text="Select File",
                command=self.open_file).grid(row=0, column=10, sticky='e' + 'w', padx=10, pady=2)
 
     @staticmethod
-    def open_file():
-        file_object = tkinter.filedialog\
-            .askopenfile(mode='r', filetypes=[("All Files", "*.*"), ("MS Word", "*.doc;*.docx")])
-        print(file_object)
+    def open_file(event=None):
+        input_file_name = tkinter.filedialog\
+            .askopenfilename(filetypes=[("All Files", "*.*"), ("MS Word", "*.doc;*.docx")])
+        if input_file_name:
+            global file_name
+            file_name = input_file_name
+            input_dir.delete(0, END)
+            input_dir.insert(0, file_name)
 
 
 parent = Tk()
