8e7103178b6412b51d5a461085325f2be8cc9eba
pluboj <pluboj@yahoo.com>
2017-11-20 00:00:18 -0800

    Add text manipulation

--- TextExtractor.py
+++ TextExtractor.py
@@ -9,4 +9,16 @@ class TextExtractor:
     def process_text(self):
         doc = Document(self.filename)
         for paragraph in doc.paragraphs:
-            print(paragraph.text)
+            item = []
+            for run in paragraph.runs:
+                if run.bold:
+                    item.append("<b>"+run.text+"</b>")
+                elif run.italic:
+                    item.append("<i>" + run.text + "</i>")
+                elif run.underline:
+                    item.append("<u>" + run.text + "</u>")
+                else:
+                    item.append(run.text)
+
+            joined_item = "".join(item)
+            print(joined_item.replace("</b><b>", ""))
