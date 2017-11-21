5d254264ddf3561627f15f520b82cd2bf1141a71
pluboj <pluboj@yahoo.com>
2017-11-21 12:46:38 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ base = None
 if sys.platform == 'win32':
     base = "Win32GUI"
 
-executables = [cx_Freeze.Executable("Main.py", base=base)]
+executables = [cx_Freeze.Executable("Main.py", base=base, icon="DC.ico")]
 
 cx_Freeze.setup(
     name="DC",
-    options={"build_exe": {"packages": ["tkinter", "docx"]}},
+    options={"build_exe": {"packages": ["tkinter", "docx"], "include_files": ["DC.ico"]}},
     version="0.01",
     executables=executables
 )
